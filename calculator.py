bef492f6f85bd2bcd210a3c8b986cd30df8cec94
Lahari Priya <laharipriya@gmail.com>
2026-09-04 14:17:22 -0700

    Initial Project

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print ('Simple calculator\n')
+
+num1 = float (input('Enter first number :'))
+num2 = float (input( 'Enter second number :'))
+
+print ('\nResults: ')
+
+print ('Addition: ', num1 + num2)
+print ('subtraction: ', num1 - num2)
+print ('multiplication: ', num1 * num2)
+
+if num2!=0:
+	print('Division: ', num1/num2)
+else:
+	print('Division: Cannot divide by Zero')
+
